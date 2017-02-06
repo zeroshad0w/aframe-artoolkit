ba7deb8f66657171520c73e631a162f5618013d5
Jerome Etienne <jerome.etienne@gmail.com>
2017-02-06 14:05:27 +0000

    more work

--- threex-armarkercontrols.js
+++ threex-armarkercontrols.js
@@ -26,7 +26,7 @@ THREEx.ArMarkerControls = function(context, object3d, parameters){
 	this.markerId = null
 
         // create the marker Root
-	this.object3d = object3d || new THREE.Group
+	this.object3d = object3d
 	this.object3d.name = 'Marker Root'
 	this.object3d.matrixAutoUpdate = false;
 	this.object3d.visible = false
